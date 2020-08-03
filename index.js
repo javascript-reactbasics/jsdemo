6ca86cdb63be33239a3a1cb015ff4dfd05970628
shubham dixit <shubhamdixit863@gmail.com>
2020-08-03 22:28:10 +0530

    Project Initiated

---
+++ index.js
@@ -0,0 +1,37 @@
+/// al your javascript stuffs
+
+//alert("hello there");
+
+
+/// javascript fundamentals 
+
+// there are two types of data in javascript
+
+//1-primitive types 
+// number ,string ,boolean ,undefined ,null ,symbols (introduced in es6);
+// number 1 ,2 ,3 ,4
+// string "" or '' i sstring for  javscriptt ;
+// boolean true or false
+
+// undefined // where you declared a variable and did n't give  it a value
+// null means nothing 
+// symbols --> are quite complicate ddatattype they are geneally function geneart
+
+
+// Apart from this   everything in js is object 
+
+// object.prototupes 
+
+
+
+/// DOM ////
+
+//DATA OBJECT MODEL 
+
+// we have some properties for document object model
+
+//document.getElementById("id_of_html_element");
+// document.getElementsByClassName("class_name");
+// document.querySelector("#")  -->for id
+// document.querySelector(".classname") -> for class
+// docuemt.querySelectorAll ()
